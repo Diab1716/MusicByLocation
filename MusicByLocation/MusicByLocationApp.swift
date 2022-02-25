67eb561fa107f30bc518206b61c28f5dc26b4086
Diab1716 <diab.ahmed@etoncollege.org.uk>
2022-02-25 19:50:24 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Diab, Ahmed (HWTA) on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
